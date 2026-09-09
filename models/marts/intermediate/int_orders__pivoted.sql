c2cc4129218ef106c44566d9269557e65eaadfb3
Cyprien Boullie <boulliec11@gmail.com>
2026-09-09 15:05:19 +0000

    int_orders__pivoted.sql pour pratiquer du Jinja

---
+++ models/marts/intermediate/int_orders__pivoted.sql
@@ -0,0 +1,25 @@
+{% set payment_methods= ['credit_card','gift_card','coupon','bank_transfer'] %} 
+
+with
+
+payments as (
+    select * from {{ ref('stg_stripe__payment') }}
+    where payment_status = 'success'
+),
+
+pivoted as (  
+    select
+    order_id,
+
+        {%- for payment_method in payment_methods -%}
+            sum(case when payment_method = '{{ payment_method }}' then payment_amount end) as {{ payment_method }}_amount
+            {%- if not loop.last -%}
+            ,
+            {% endif %}
+            
+        {% endfor %}
+
+    from payments
+    group by 1
+    )
+    select * from pivoted
